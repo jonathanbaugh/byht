f6a38f455fb56ace64168182e7cfdb4c34b3095e
Jonathan Baugh <jonathanatx@gmail.com>
2019-06-10 21:40:02 +0200

    Update paths

--- bin/byht.sh
+++ bin/byht.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-BASEDIR=$(dirname $0)
-PYTHON_PATH=$BASEDIR/../byht/venv/bin/python3
-
-$PYTHON_PATH $BASEDIR/../byht/byht.py $@
+~/.byht/byht/venv/bin/python3 ~/.byht/byht/byht.py $@
